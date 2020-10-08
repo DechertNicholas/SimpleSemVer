b2c346a280c9590061177dc60425d8ee6fa53efc
Nicholas Dechert <dechert.nicholas@gmail.com>
2020-10-08 13:47:22 -0700

    Test results

--- AzureHelperFunctions/RunPester.ps1
+++ AzureHelperFunctions/RunPester.ps1
@@ -4,5 +4,11 @@ $testDir = Resolve-Path "$here\..\test"
 Write-Host "Tests in $testDir"
 Import-Module Pester
 Get-Module Pester
-Invoke-Pester -Script "$testDir\*"
+if ($env:TF_BUILD) {
+    Invoke-Pester -Script "$testDir\*" -OutputFile "$($env:Common_TestResultsDirectory)/TestResults.xml" -OutputFormat "XUnitXml" -
+}
+else {
+    Invoke-Pester -Script "$testDir\*"
+}
+
 Remove-Module Pester -ErrorAction SilentlyContinue
