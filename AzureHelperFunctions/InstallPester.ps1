71236caf24eef3d5bb00b647bc317e0443fd33af
Nicholas Dechert <dechert.nicholas@gmail.com>
2020-10-07 18:45:11 -0700

    Debug

--- AzureHelperFunctions/InstallPester.ps1
+++ AzureHelperFunctions/InstallPester.ps1
@@ -1,9 +1,13 @@
+Write-Output "Checking if pester is installed..."
 $modules = Get-Module -ListAvailable
 if ($modules.Name -notcontains 'pester') {
+    Write-Output "Not found, installing pester"
     Install-Module -Name Pester -Force -SkipPublisherCheck
+
+    Write-Output "Verifying installation..."
+    $modules = Get-Module -ListAvailable
+    if ($modules.Name -notcontains 'pester') {
+        throw "Unable to install Pester`n$($modules.Name)"
+    }
 }
-$modules = Get-Module -ListAvailable
-if ($modules.Name -notcontains 'pester') {
-    throw "Unable to install Pester`n$($modules.Name)"
-}
-Get-Module 'Pester'
+Write-Output "$(Get-Module 'Pester')"
