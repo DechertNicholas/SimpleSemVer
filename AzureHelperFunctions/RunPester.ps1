809a15aa2f19607e25c762bebf558a4a8a35840d
Nicholas Dechert <dechert.nicholas@gmail.com>
2020-10-09 10:28:40 -0700

    Code coverage

--- AzureHelperFunctions/RunPester.ps1
+++ AzureHelperFunctions/RunPester.ps1
@@ -1,11 +1,12 @@
 $ErrorActionPreference = "Stop"
 $here = (Split-Path -Parent $MyInvocation.MyCommand.Path)
 $testDir = Resolve-Path "$here\..\test"
+$srcDir = Resolve-Path "$here\..\src"
 Write-Host "Tests in $testDir"
 Import-Module Pester
 Get-Module Pester
 if ($env:TF_BUILD) {
-    Invoke-Pester -Script "$testDir\*" -OutputFile "$($env:Common_TestResultsDirectory)/TestResults.xml" -OutputFormat NUnitXml
+    Invoke-Pester -Script "$testDir\*" -OutputFile "$($env:Common_TestResultsDirectory)/TestResults.xml" -OutputFormat NUnitXml -CodeCoverage $srcDir\SimpleSemVer.psm1 -CodeCoverageOutputFile "$($env:Common_TestResultsDirectory)/CodeCov.xml" -CodeCoverageOutputFileFormat JaCoCo
 }
 else {
     Invoke-Pester -Script "$testDir\*"
